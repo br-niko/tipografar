07ce626f333275c87232c3feabb7842e93bc6b4c
Nikolas <nikolaslorencini@gmail.com>
2025-11-10 14:33:51 -0300

    search review

--- js/search.js
+++ js/search.js
@@ -1,54 +1,15 @@
-(function () {
-  function initLunr() {
-    fetch("/search.json")
-      .then((response) => response.json())
-      .then((data) => {
-        window.searchData = data;
-        window.lunrIdx = lunr(function () {
-          this.ref("id");
-          this.field("title", { boost: 10 });
-          this.field("aluno");
-          this.field("tags");
-          this.field("content");
-          data.forEach(function (doc, idx) {
-            doc.id = idx;
-            this.add(doc);
-          }, this);
-        });
-      })
-      .catch((error) =>
-        console.error("Erro ao carregar o search.json:", error),
-      );
-  }
-  function showSearchResults() {
-    const query = this.value;
-    const resultsContainer = document.getElementById("search-results");
-    if (!query || query.length < 2 || !window.lunrIdx) {
-      resultsContainer.innerHTML = "";
-      resultsContainer.style.display = "none";
-      return;
+---
+layout: null
+---
+[
+  {% for projeto in site.posts %}
+    {
+      "title": "{{ projeto.title | escape }}",
+      "aluno": "{{ projeto.aluno | escape }}",
+      "url": "{{ projeto.url | relative_url }}",
+      "tags": "{{ projeto.tags | join: ', ' }}",
+      "content": {{ projeto.content | strip_html | strip_newlines | jsonify }}
     }
-    const results = window.lunrIdx.search(query + "* " + query);
-    let output = '<ul class="list-group">';
-    const uniqueResults = results.filter(
-      (result, index, self) =>
-        index === self.findIndex((t) => t.ref === result.ref),
-    );
-    if (uniqueResults.length === 0) {
-      output += '<li class="list-group-item">Nenhum resultado encontrado.</li>';
-    } else {
-      uniqueResults.slice(0, 5).forEach(function (result) {
-        const doc = window.searchData[result.ref];
-        output += `<li class="list-group-item"><a href="${doc.url}">${doc.title}</a><br><small>${doc.aluno}</small></li>`;
-      });
-    }
-    output += "</ul>";
-    resultsContainer.innerHTML = output;
-    resultsContainer.style.display = "block";
-  }
-  initLunr();
-  const searchInput = document.getElementById("search-input");
-  if (searchInput) {
-    searchInput.addEventListener("keyup", showSearchResults);
-  }
-})();
+    {% unless forloop.last %},{% endunless %}
+  {% endfor %}
+]
